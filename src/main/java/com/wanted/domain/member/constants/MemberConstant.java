bc43d17e650c6feb3c981f091b2ff03bf4d7ea5a
choisoyeong <thdud3410@gmail.com>
2023-11-23 23:11:28 +0900

    feat: 회원가입시 id, password 입력 관련 제한값 상수로 분리

---
+++ src/main/java/com/wanted/domain/member/constants/MemberConstant.java
@@ -0,0 +1,8 @@
+package com.wanted.domain.member.constants;
+
+public class MemberConstant {
+
+    public static final int ACCOUNT_MIN_LENGTH = 6;
+    public static final int ACCOUNT_MAX_LENGTH = 20;
+    public static final int PASSWORD_MAX_LENGTH = 256;
+}
