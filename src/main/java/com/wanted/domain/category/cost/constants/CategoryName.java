78141dbff2197aaad4f78349d7c8921357d342fd
choisoyeong <thdud3410@gmail.com>
2023-11-28 21:32:50 +0900

    feat: 예산 카테고리 enum 추가

---
+++ src/main/java/com/wanted/domain/category/cost/constants/CategoryName.java
@@ -0,0 +1,15 @@
+package com.wanted.domain.category.cost.constants;
+
+public enum CategoryName {
+    Food,
+    Transportation,
+    Cultural,
+    GroceryShopping,
+    Fashion,
+    Housing,
+    Phone,
+    Healthcare,
+    Education,
+    Etc
+
+}
--- src/main/java/com/wanted/domain/category/cost/entity/CostCategory.java
+++ src/main/java/com/wanted/domain/category/cost/entity/CostCategory.java
@@ -1,12 +1,9 @@
 package com.wanted.domain.category.cost.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import com.wanted.domain.category.cost.constants.CategoryName;
+import jakarta.persistence.*;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -26,6 +23,13 @@ public class CostCategory {
     private Long id;
 
     // 카테고리 이름
+    @Enumerated(EnumType.STRING)
     @Column(name = "name", nullable = false)
-    private String name;
+    private CategoryName name;
+
+    @Builder
+    private CostCategory(Long id, CategoryName name) {
+        this.id = id;
+        this.name = name;
+    }
 }
