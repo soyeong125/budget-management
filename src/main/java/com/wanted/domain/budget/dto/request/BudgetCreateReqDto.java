4e71d89228e1fd0977c307124638d49a24a8cf05
choisoyeong <thdud3410@gmail.com>
2023-11-30 23:51:06 +0900

    style: 사용하지 않는 import문 삭제

--- src/main/java/com/wanted/domain/budget/dto/request/BudgetCreateReqDto.java
+++ src/main/java/com/wanted/domain/budget/dto/request/BudgetCreateReqDto.java
@@ -1,5 +1,7 @@
 package com.wanted.domain.budget.dto.request;
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PositiveOrZero;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -10,8 +12,11 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor
 public class BudgetCreateReqDto {
+    @NotNull(message = "사용자 ID를 입력해주세요")
     private Long memberId;
+    @NotNull(message = "카테고리를 선택해주세요")
     private String categoryName;
+    @PositiveOrZero(message = "예산은 0 이상이어야 합니다")
     private Integer cost;
 
     @Builder
--- src/main/java/com/wanted/domain/member/entity/Member.java
+++ src/main/java/com/wanted/domain/member/entity/Member.java
@@ -1,5 +1,6 @@
 package com.wanted.domain.member.entity;
 
+import com.wanted.domain.budget.entity.Budget;
 import com.wanted.global.entity.BaseTimeEntity;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
@@ -7,9 +8,13 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static com.wanted.domain.member.constants.MemberConstant.ACCOUNT_MAX_LENGTH;
 import static com.wanted.domain.member.constants.MemberConstant.PASSWORD_MAX_LENGTH;
 import static jakarta.persistence.EnumType.STRING;
+import static jakarta.persistence.FetchType.LAZY;
 import static jakarta.persistence.GenerationType.IDENTITY;
 
 /**
@@ -37,7 +42,7 @@ public class Member extends BaseTimeEntity {
     private Role role;
 
     @Builder
-    public Member(Long id, String account, String password, Role role) {
+    public Member(Long id, String account, String password, Role role,) {
         this.id = id;
         this.account = account;
         this.password = password;
