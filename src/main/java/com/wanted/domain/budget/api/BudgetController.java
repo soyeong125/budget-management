a7d0aed9b44d87d6a097d515afe04da575de0687
choisoyeong <thdud3410@gmail.com>
2023-11-30 23:50:09 +0900

    feat: 예산 추천 기능 구현

--- src/main/java/com/wanted/domain/budget/api/BudgetController.java
+++ src/main/java/com/wanted/domain/budget/api/BudgetController.java
@@ -2,13 +2,13 @@ package com.wanted.domain.budget.api;
 
 import com.wanted.domain.budget.application.BudgetService;
 import com.wanted.domain.budget.dto.request.BudgetCreateReqDto;
+import com.wanted.domain.budget.dto.request.BudgetRecommendReqDto;
+import com.wanted.domain.budget.dto.response.BudgetRecommendResDto;
 import com.wanted.global.format.response.ResponseApi;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -24,8 +24,25 @@ public class BudgetController {
      * @return 생성된 예산 Id
      */
     @PostMapping
-    public ResponseEntity<ResponseApi> createBudget(@RequestBody BudgetCreateReqDto reqDto) {
+    public ResponseEntity<ResponseApi> createBudget(
+            @RequestBody @Valid BudgetCreateReqDto reqDto) {
         Long budgetId = budgetService.createBudget(reqDto);
         return ResponseEntity.ok(ResponseApi.toSuccessForm(budgetId));
+    }
+
+    /**
+     * 예산 설계 추천
+     * 예산 생성 기준은 기존 이용중인 사용자들이 설정한 평균 값.
+     *
+     * @param reqDto 예산 추천 요청 dto
+     * @return 예산 설계 추천 결과
+     */
+    @GetMapping("/recommend")
+    public ResponseEntity<ResponseApi> recommendDeposit(
+            @Valid @RequestBody BudgetRecommendReqDto reqDto
+    ) {
+        BudgetRecommendResDto resDto = budgetService.createBudgetRecommendation(reqDto);
+
+        return ResponseEntity.ok(ResponseApi.toSuccessForm(resDto));
     }
 }
--- src/main/java/com/wanted/domain/budget/application/BudgetService.java
+++ src/main/java/com/wanted/domain/budget/application/BudgetService.java
@@ -2,7 +2,10 @@ package com.wanted.domain.budget.application;
 
 import com.wanted.domain.budget.dao.BudgetRepository;
 import com.wanted.domain.budget.dto.request.BudgetCreateReqDto;
+import com.wanted.domain.budget.dto.request.BudgetRecommendReqDto;
+import com.wanted.domain.budget.dto.response.BudgetRecommendResDto;
 import com.wanted.domain.budget.entity.Budget;
+import com.wanted.domain.category.cost.constants.CategoryName;
 import com.wanted.domain.category.cost.dao.CostCategoryRepository;
 import com.wanted.domain.category.cost.entity.CostCategory;
 import com.wanted.domain.member.dao.MemberRepository;
@@ -12,6 +15,11 @@ import com.wanted.global.error.ErrorCode;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static com.wanted.global.error.ErrorCode.MEMBER_NOT_FOUND;
 
 @Service
 @Transactional
@@ -30,9 +38,7 @@ public class BudgetService {
      */
     public Long createBudget(BudgetCreateReqDto reqDto) {
         // 요청 dto로부터 사용자를 조회한다.
-        Member member = memberRepository.findById(reqDto.getMemberId()).orElseThrow(
-                () -> new BusinessException(reqDto.getMemberId(), "memberId", ErrorCode.MEMBER_NOT_FOUND)
-        );
+        Member member = findMemberById(reqDto.getMemberId());
 
         // 요청 dto로부터 예산 카테고리를 조회한다.
         CostCategory category = costCategoryRepository.findByName(reqDto.getCategoryName()).orElseThrow(
@@ -49,4 +55,64 @@ public class BudgetService {
 
         return savedBudget.getId();
     }
+
+    /**
+     * 예산 설계 추천
+     * 예산 생성 기준은 기존 이용중인 사용자들이 설정한 평균 값.
+     *
+     * @param reqDto 예산 추천 요청 dto
+     * @return 예산 설계 추천 결과
+     */
+    public BudgetRecommendResDto createBudgetRecommendation(BudgetRecommendReqDto reqDto) {
+
+        // 요청 dto로부터 사용자를 조회한다.
+        Member member = findMemberById(reqDto.getMemberId());
+
+        // 기존 이용중인 사용자들이 설정한 예산 비율의 평균을 계산
+        Map<CategoryName, Double> averageBudgetRates = calculateAverageBudgetRates();
+
+        // 예산 총액을 카테고리 별 비율로 나눈다.
+        Map<CategoryName, Integer> budgetRates = new HashMap<>();
+        for (CategoryName categoryName : CategoryName.values()) {
+            double rate = averageBudgetRates.getOrDefault(categoryName, 0.0);
+            if (rate < 0.1) {
+                rate = 0; // 10% 미만인 경우, ETC 카테고리로 처리
+            }
+            int budgetAmount = (int) (reqDto.getTotalBudgetAmount() * rate);
+            budgetAmount = budgetAmount / 100 * 100; // 100원 단위로 반올림한다.
+            budgetRates.put(categoryName, budgetAmount);
+        }
+
+        return new BudgetRecommendResDto(budgetRates);
+    }
+
+    //TODO
+
+    /**
+     * 기존 사용자들이 설정한 예산 비율의 평균 계산
+     *
+     * @return 카테고리별 예산 비율
+     */
+    private Map<CategoryName, Double> calculateAverageBudgetRates() {
+        // 데이터베이스에서 사용자별 카테고리 예산 데이터를 가져와서 평균을 계산하는 로직
+        Map<CategoryName, Double> averageRates = new HashMap<>();
+        // 데이터베이스 조회 및 평균 계산 로직 구현...
+        return averageRates;
+    }
+
+
+    /**
+     * Id로 사용자 조회
+     *
+     * @param id
+     * @return 사용자
+     */
+    public Member findMemberById(Long id){
+        Member member = memberRepository.findById(id).orElseThrow(
+                () -> new BusinessException(id, "memberId", MEMBER_NOT_FOUND)
+        );
+
+        return member;
+    }
+
 }
