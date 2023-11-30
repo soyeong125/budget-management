805965f69c0b829b1e32571b8d04c297ed187d1a
choisoyeong <thdud3410@gmail.com>
2023-12-01 00:43:09 +0900

    feat: 지출 entity 및 dao 생성

---
+++ src/main/java/com/wanted/domain/expenditure/entity/Expenditure.java
@@ -0,0 +1,60 @@
+package com.wanted.domain.expenditure.entity;
+
+import static jakarta.persistence.FetchType.LAZY;
+
+import com.wanted.domain.category.cost.entity.CostCategory;
+import com.wanted.domain.member.entity.Member;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import java.time.LocalDateTime;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+/**
+ * 지출
+ */
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@Table(name = "expenditure")
+public class Expenditure {
+
+    // 지출 id
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "expenditure_id", nullable = false)
+    private Long id;
+
+    // 비용 카테고리 (N:1)
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "cost_category_id", nullable = false)
+    private CostCategory category;
+
+    // 회원 (N:1)
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "member_id", nullable = false)
+    private Member member;
+
+    // 지출 금액
+    @Column(name = "cost", nullable = false)
+    private Integer cost;
+
+    // 메모
+    @Column(name = "memo", length = 100, nullable = false)
+    private String memo;
+
+    // 합계 제외 여부
+    @Column(name = "is_excluded", nullable = false)
+    private Boolean isExcluded;
+
+    // 지출 시간
+    @Column(name = "time", nullable = false)
+    private LocalDateTime time;
+}
